0cb8bf14e7f6e769d7432e322fd6e4ad870700a1
nordluma <maxnordlund.95@hotmail.com>
2024-02-29 22:39:59 +0200

    feat(cdn): setup http server

--- cdn/src/main.rs
+++ cdn/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{response::IntoResponse, routing::get, Router};
+use tracing::debug;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let app = Router::new().route("/", get(index));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+
+    debug!("listening on {}", addr);
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn index() -> impl IntoResponse {
+    "Hello, world"
 }
